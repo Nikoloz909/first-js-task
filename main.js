a9bacf59f121bd3a4ef0a84b803416348cc6d2d2
Nikoloz <nikoloz.kvachantiradze17b@geolab.edu.ge>
2021-05-04 15:28:22 +0400

    add new task

--- main.js
+++ main.js
@@ -1,63 +1,76 @@
 let arr = ['javascript', 'php', 'c#', 'python', 'c++', 'kotlin', 'c', 'swift'];
+for (let i = 0; i < arr.length; i++) {
 
-for(let i = 0 ; i < arr.length; i++){
+    if (arr[i].length < 5) {
 
-  if(arr[i].length < 5){
-
-    continue;
-
-  }
-
-  console.log(arr[i]);
-
-}
-
-
-let numbers = [2, -3, 5, 11, 1, -30, -11, 100, -1, -3, -4];
-
-for(let i = 0; i < numbers.length; i++){
-   if(numbers[i] < 0){
-
-     continue;
-
-   }
-   console.log(numbers[i])
-}
-
-let numbersOne = [[2, -3, 5, 11], [1, -30, -11, 100], [-1, -3, -4]];
-for(let i = 0; i < numbersOne.length; i++){
-
-  let numbersTwo = numbersOne[i]
-
-  for(let x = 0; x < numbersTwo.length; x++){
-
-    if(numbersTwo[x] < 0){
-
-      continue;
-
+        continue;
     }
 
-    console.log(numbersTwo[x]);
-  }
+    console.log(arr[i])
 }
+
+let numbers = [2, -3, 5, 11, 1, -30, -11, 100, -1, -3, -4];
+for (let i = 0; i < numbers.length; i++) {
+
+    if (numbers[i] < 0) {
+
+        continue;
+    }
+    console.log(numbers[i])
+}
+
+
+let secNumber = [
+    [2, -3, 5, 11],
+    [1, -30, -11, 100],
+    [-1, -3, -4]
+];
+
+for (let i = 0; i < secNumber.length; i++) {
+
+    const element = secNumber[i];
+    for (let x = 0; x < element.length; x++) {
+
+        if (element[x] < x) {
+
+            continue;
+        }
+
+         console.log(element[x])
+    }
+}
+
+let maxValue = [1, 5, 30, 6, 7, 523];
+
+function myFunciton(maxValue) {
+
+    let numbers = 0;
+
+    for (let i = 0; i < maxValue.length; i++) {
+        let x = maxValue[i];
+        if (x > numbers) {
+            numbers = x
+        }
+    }
+    return numbers
+}
+
+
+     console.log(myFunciton(maxValue))
+
 
 let user = {
-  firstname: 'giorgi',
-  lastname: 'saakadze',
-  age: 32,
-  isLoggedIn: true
+    firstname: 'giorgi',
+    lastname: 'saakadze',
+    age: 32,
+    isLoggedIn: true
 }
 
-function printName (obj){
-
-  if(obj.isLoggedIn){
-
-    return obj.firstname + ': ' + obj.lastname + ": " + obj.age;
-
-  }
-
-  return false;
+function getFunction(user) {
+    if (user.isLoggedIn) {
+        return user.firstname + ': ' + user.lastname
+    }
+    return false
 }
-
-
-console.log(printName(user))
+getFunction(user)
+console.log(getFunction(user))
